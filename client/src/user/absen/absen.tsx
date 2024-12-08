cc32373bb42db23bfcdc0e71a03bebf90b3484e0
Abiyoga Permana Chandra <abiyogachandra@gmail.com>
2024-12-08 20:59:40 +0700

    Camera

--- client/src/user/absen/absen.tsx
+++ client/src/user/absen/absen.tsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react';
 // import assets from '../../assets/assets.ts';
 import Sidebar from "../../components/sidebar"
+import Camera from "../../components/camera"
 
 function Absen() {
   return (
@@ -9,12 +10,7 @@ function Absen() {
         <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 h-full">
           <Sidebar active="Absen Sekarang" />
         </div>
-        <div className="w-full pt-[78px]">
-          <div className="p-12">
-            <h1 className="text-3xl font-bold">Maaf! Halaman ini masih belum dibuat.</h1>
-            <p className="opacity-50 mt-1">Absen Sekarang</p>
-          </div>
-        </div>
+        <Camera />
       </div>
     </>
   )

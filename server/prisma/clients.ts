c347ad9770cad507eeed019644d31fc50a84636c
Abiyoga Permana Chandra <abiyogachandra@gmail.com>
2024-12-10 12:54:37 +0700

    CRUD Guest

--- server/prisma/clients.ts
+++ server/prisma/clients.ts
@@ -5,3 +5,4 @@ export const studentPrisma = new PrismaClient().students;
 export const attendancePrisma = new PrismaClient().attendances;
 export const inattendancePrisma = new PrismaClient().inattendances;
 export const violationPrisma = new PrismaClient().violations;
+export const guestPrisma = new PrismaClient().guests;

2603a4bdaa03db964ffe8cfc189c51192c4685bf
Abiyoga Permana Chandra <abiyogachandra@gmail.com>
2024-12-10 13:00:36 +0700

    Added enum validation to Inattendance's reason

--- server/src/utils/schemas/inattendance/update.ts
+++ server/src/utils/schemas/inattendance/update.ts
@@ -1,10 +1,11 @@
 import Joi from "joi";
+import { InattendanceReasonEnum } from "../../enums/InattendanceReason.js";
 
 export default function schema() {
     return Joi.object({
         id: Joi.string().required(),
         student_id: Joi.string().required(),
-        reason: Joi.string().required(),
+        reason: Joi.string().required().valid(Object.keys(InattendanceReasonEnum)),
         evidence_photo_path: Joi.string().required(),
     }); 
 }
